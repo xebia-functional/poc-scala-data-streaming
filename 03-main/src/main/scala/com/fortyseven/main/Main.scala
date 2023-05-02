2f99ec56056abd5a64e1c3ec0373fbd277e1c5f5
David <103842788+dagmendez@users.noreply.github.com>
2023-05-02 14:29:35 +0200

    Check Style and fix year on the headers from 2010 to 2023

--- 03-main/src/main/scala/com/fortyseven/main/Main.scala
+++ 03-main/src/main/scala/com/fortyseven/main/Main.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Xebia Functional
+ * Copyright 2023 Xebia Functional
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package com.fortyseven.main
 
 object Main:
+
   def main(args: Array[String]): Unit = Program.run(
     "This is a test!"
   )
-
--- 03-main/src/main/scala/com/fortyseven/main/Program.scala
+++ 03-main/src/main/scala/com/fortyseven/main/Program.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Xebia Functional
+ * Copyright 2023 Xebia Functional
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,4 +17,5 @@
 package com.fortyseven.main
 
 object Program:
+
   def run(configuration: String): Unit = println(configuration)
