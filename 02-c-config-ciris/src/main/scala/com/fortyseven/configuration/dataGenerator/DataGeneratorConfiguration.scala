5192b1044cc17c63931fbe918c6f998f9d85df1a
David <103842788+dagmendez@users.noreply.github.com>
2023-05-09 11:07:24 +0200

    Add datagenerator configuration

---
+++ 02-c-config-ciris/src/main/scala/com/fortyseven/configuration/dataGenerator/DataGeneratorConfiguration.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 Xebia Functional
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fortyseven.configuration.dataGenerator
+
+import cats.effect.Async
+import cats.syntax.all.*
+import ciris.{ConfigValue, Effect}
+import com.fortyseven.coreheaders.config.ConfigurationHeader
+
+case class DataGeneratorConfiguration (
+  gpsPosition: GpsPosition,
+  pneumaticPressure: PneumaticPressure,
+  producer: Producer
+)
+
+object DataGeneratorConfiguration:
+  val config: ConfigValue[Effect, DataGeneratorConfiguration] =
+    (
+      GpsPosition.config,
+      PneumaticPressure.config,
+      Producer.config
+    ).parMapN(DataGeneratorConfiguration.apply)
+
+final class DataGeneratorConfigurationEffect[F[_] : Async] extends ConfigurationHeader[F, DataGeneratorConfiguration]:
+
+  override def configuration: F[DataGeneratorConfiguration] = DataGeneratorConfiguration.config.load[F]
