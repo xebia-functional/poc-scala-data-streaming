57b56ee5d4fe5a4c9952012322a8da6968395a0e
David <103842788+dagmendez@users.noreply.github.com>
2023-05-24 21:03:57 +0200

    Implements the example of the Spark site https://spark.apache.org/docs/latest/streaming-programming-guide.html

--- 02-o-processor-spark/src/main/scala/com/fortyseven/processor/spark/HelloWorld.scala
+++ 02-o-processor-spark/src/main/scala/com/fortyseven/processor/spark/HelloWorld.scala
@@ -1,19 +1,40 @@
 package com.fortyseven.processor.spark
 
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions.*
 import org.apache.spark.sql.SparkSession
 
 object HelloWorld:
 
   @main def run =
-    val spark = SparkSession.builder
-      .appName("HelloWorld")
-      .master(sys.env.getOrElse("SPARK_MASTER_URL", "local[*]"))
-      .getOrCreate() // 1
-    import spark.implicits.* // 2
+    val spark = SparkSession
+      .builder
+      .appName("StructuredNetworkWordCount")
+      .config("spark.master", "local")
+      .getOrCreate()
 
-    val df = List("hello", "world").toDF // 3
-    df.show() // 4
 
-    spark.stop
+    import spark.implicits.*
+    // Create DataFrame representing the stream of input lines from connection to localhost:9999
+    val lines = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 9999)
+      .load()
+
+    // Split the lines into words
+    val words = lines.as[String].flatMap(_.split(" "))
+
+    // Generate running word count
+    val wordCounts = words.groupBy("value").count()
+
+    // Start running the query that prints the running counts to the console
+    val query = wordCounts.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+
+    query.awaitTermination()
+
 
 
