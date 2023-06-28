c4d584b2c2e18afaba9a8b0daa61c7842679cd55
David <103842788+dagmendez@users.noreply.github.com>
2023-06-28 16:24:06 +0200

    Delete unnecessary scripts

--- 02-o-processor-spark/docker/start-spark-cluster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Create the directory where we will put the jar we want to submit to the cluster
-mkdir -p ./spark-apps
-
-# Start a master node and 2 workers
-docker compose up -d --scale spark-worker=2
