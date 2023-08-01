6cd1507bdaca5ccf2b15478d1f372812ba7761df
David <103842788+dagmendez@users.noreply.github.com>
2023-08-02 01:25:33 +0200

    Document refinedTypes.scala

--- 01-c-core/src/main/scala/com/fortyseven/coreheaders/model/types/refinedTypes.scala
+++ 01-c-core/src/main/scala/com/fortyseven/coreheaders/model/types/refinedTypes.scala
@@ -20,6 +20,23 @@ import scala.compiletime.{error, requireConst}
 
 import com.fortyseven.coreheaders.model.iot.errors.*
 
+/**
+ * NameSpace for the following instances:
+ *
+ *   - [[com.fortyseven.coreheaders.model.types.refinedTypes.Bar]]
+ *
+ *   - [[com.fortyseven.coreheaders.model.types.refinedTypes.Hz]]
+ *
+ *   - [[com.fortyseven.coreheaders.model.types.refinedTypes.Latitude]]
+ *
+ *   - [[com.fortyseven.coreheaders.model.types.refinedTypes.Longitude]]
+ *
+ *   - [[com.fortyseven.coreheaders.model.types.refinedTypes.Meters]]
+ *
+ *   - [[com.fortyseven.coreheaders.model.types.refinedTypes.Percentage]]
+ *
+ *   - [[com.fortyseven.coreheaders.model.types.refinedTypes.Speed]]
+ */
 object refinedTypes:
 
   opaque type Latitude = Double
@@ -36,12 +53,34 @@ object refinedTypes:
 
   opaque type Meters = Int
 
+  /**
+   * Factory for [[com.fortyseven.coreheaders.model.types.refinedTypes.Latitude]] instances.
+   */
   object Latitude:
 
+    /**
+     * Tries to build a valid value of type Latitude from the given double.
+     *
+     * @param coordinateCandidate
+     *   the given Double that could be a Latitude.
+     * @return
+     *   Either a throwable of type OutOfBoundsError or a valid instance of type Latitude.
+     */
     def from(coordinateCandidate: Double): Either[OutOfBoundsError, Latitude] = coordinateCandidate match
       case c if c < -90.0 || c > 90.0 => Left(OutOfBoundsError(s"Invalid latitude value $c"))
       case c                          => Right(c)
 
+    /**
+     * Method that builds a Latitude from a double at compile time. This method is used when working with fixed values (''magic numbers'') in the
+     * code. The compiler will warn us if the value is invalid at compile time.
+     *
+     * @param coordinate
+     *   the given Double.
+     * @return
+     *   A valid Latitude or a compiler error.
+     * @see
+     *   More info at [[https://docs.scala-lang.org/scala3/reference/metaprogramming/inline.html]]
+     */
     inline def apply(coordinate: Double): Latitude =
       requireConst(coordinate)
       inline if coordinate < -90.0 || coordinate > 90.0
@@ -50,12 +89,34 @@ object refinedTypes:
 
     extension (coordinate: Latitude) def value: Double = coordinate
 
+  /**
+   * Factory for [[com.fortyseven.coreheaders.model.types.refinedTypes.Longitude]] instances.
+   */
   object Longitude:
 
+    /**
+     * Tries to build a valid value of type Longitude from the given double.
+     *
+     * @param coordinateCandidate
+     *   the given Double that could be a Longitude.
+     * @return
+     *   Either a throwable of type OutOfBoundsError or a valid instance of type Longitude.
+     */
     def from(coordinateCandidate: Double): Either[OutOfBoundsError, Longitude] = coordinateCandidate match
       case c if c < -180.0 || c > 180.0 => Left(OutOfBoundsError(s"Invalid longitude value $c"))
       case c                            => Right(c)
 
+    /**
+     * Method that builds a Longitude from a double at compile time. This method is used when working with fixed values (''magic numbers'') in the
+     * code. The compiler will warn us if the value is invalid at compile time.
+     *
+     * @param coordinate
+     *   the given Double.
+     * @return
+     *   A valid Longitude or a compiler error.
+     * @see
+     *   More info at [[https://docs.scala-lang.org/scala3/reference/metaprogramming/inline.html]]
+     */
     inline def apply(coordinate: Double): Longitude =
       requireConst(coordinate)
       inline if coordinate < -180.0 || coordinate > 180.0
@@ -64,12 +125,34 @@ object refinedTypes:
 
     extension (coordinate: Longitude) def value: Double = coordinate
 
+  /**
+   * Factory for [[com.fortyseven.coreheaders.model.types.refinedTypes.Percentage]] instances.
+   */
   object Percentage:
 
+    /**
+     * Tries to build a valid value of type Percentage from the given double.
+     *
+     * @param percentageCandidate
+     *   the given Double that could be a Percentage.
+     * @return
+     *   Either a throwable of type OutOfBoundsError or a valid instance of type Percentage.
+     */
     def from(percentageCandidate: Double): Either[OutOfBoundsError, Percentage] = percentageCandidate match
       case p if p < 0.0 || p > 100.0 => Left(OutOfBoundsError(s"Invalid percentage value $p"))
       case percentage                => Right(percentage)
 
+    /**
+     * Method that builds a Percentage from a double at compile time. This method is used when working with fixed values (''magic numbers'') in the
+     * code. The compiler will warn us if the value is invalid at compile time.
+     *
+     * @param percentage
+     *   the given Double.
+     * @return
+     *   A valid Percentage or a compiler error.
+     * @see
+     *   More info at [[https://docs.scala-lang.org/scala3/reference/metaprogramming/inline.html]]
+     */
     inline def apply(percentage: Double): Percentage =
       requireConst(percentage)
       inline if percentage < 0.0 || percentage > 100.0
@@ -78,12 +161,34 @@ object refinedTypes:
 
     extension (percentage: Percentage) def value: Double = percentage
 
+  /**
+   * Factory for [[com.fortyseven.coreheaders.model.types.refinedTypes.Speed]] instances.
+   */
   object Speed:
 
+    /**
+     * Tries to build a valid value of type Speed from the given double.
+     *
+     * @param speedCandidate
+     *   the given Double that could be a Speed.
+     * @return
+     *   Either a throwable of type OutOfBoundsError or a valid instance of type Speed.
+     */
     def from(speedCandidate: Double): Either[OutOfBoundsError, Speed] = speedCandidate match
       case speed if speed < 0.0 => Left(OutOfBoundsError(s"Invalid speed value $speed"))
       case speed                => Right(speed)
 
+    /**
+     * Method that builds a Speed from a double at compile time. This method is used when working with fixed values (''magic numbers'') in the code.
+     * The compiler will warn us if the value is invalid at compile time.
+     *
+     * @param speed
+     *   the given Double.
+     * @return
+     *   A valid Speed or a compiler error.
+     * @see
+     *   More info at [[https://docs.scala-lang.org/scala3/reference/metaprogramming/inline.html]]
+     */
     inline def apply(speed: Double): Speed =
       requireConst(speed)
       inline if speed < 0.0
@@ -92,12 +197,34 @@ object refinedTypes:
 
     extension (speed: Speed) def value: Double = speed
 
+  /**
+   * Factory for [[com.fortyseven.coreheaders.model.types.refinedTypes.Hz]] instances.
+   */
   object Hz:
 
+    /**
+     * Tries to build a valid value of type Hz from the given double.
+     *
+     * @param hertzCandidate
+     *   the given Double that could be a Hz.
+     * @return
+     *   Either a throwable of type OutOfBoundsError or a valid instance of type Hz.
+     */
     def from(hertzCandidate: Double): Either[OutOfBoundsError, Hz] = hertzCandidate match
       case hz if hz < 0.0 => Left(OutOfBoundsError(s"Invalid frequency value $hz"))
       case hz             => Right(hz)
 
+    /**
+     * Method that builds a Hz from a double at compile time. This method is used when working with fixed values (''magic numbers'') in the code. The
+     * compiler will warn us if the value is invalid at compile time.
+     *
+     * @param hertz
+     *   the given Double.
+     * @return
+     *   A valid Hz or a compiler error.
+     * @see
+     *   More info at [[https://docs.scala-lang.org/scala3/reference/metaprogramming/inline.html]]
+     */
     inline def apply(hertz: Double): Hz =
       inline if hertz < 0.0
       then error("Invalid frequency value. Accepted hertz values are greater than 0.0.")
@@ -105,12 +232,34 @@ object refinedTypes:
 
     extension (hertz: Hz) def value: Double = hertz
 
+  /**
+   * Factory for [[com.fortyseven.coreheaders.model.types.refinedTypes.Bar]] instances.
+   */
   object Bar:
 
+    /**
+     * Tries to build a valid value of type Bar from the given double.
+     *
+     * @param barCandidate
+     *   the given Double that could be a Bar.
+     * @return
+     *   Either a throwable of type OutOfBoundsError or a valid instance of type Bar.
+     */
     def from(barCandidate: Double): Either[OutOfBoundsError, Bar] = barCandidate match
       case p if p < 0.0 => Left(OutOfBoundsError(s"Invalid pressure value $p"))
       case p            => Right(p)
 
+    /**
+     * Method that builds a Bar from a double at compile time. This method is used when working with fixed values (''magic numbers'') in the code. The
+     * compiler will warn us if the value is invalid at compile time.
+     *
+     * @param bar
+     *   the given Double.
+     * @return
+     *   A valid Bar or a compiler error.
+     * @see
+     *   More info at [[https://docs.scala-lang.org/scala3/reference/metaprogramming/inline.html]]
+     */
     inline def apply(bar: Double): Bar =
       requireConst(bar)
       inline if bar < 0.0
@@ -119,12 +268,34 @@ object refinedTypes:
 
     extension (bar: Bar) def value: Double = bar
 
+  /**
+   * Factory for [[com.fortyseven.coreheaders.model.types.refinedTypes.Meters]] instances.
+   */
   object Meters:
 
+    /**
+     * Tries to build a valid value of type Meters from the given int.
+     *
+     * @param metersCandidate
+     *   the given Int that could be a Meters.
+     * @return
+     *   Either a throwable of type OutOfBoundsError or a valid instance of type Meters.
+     */
     def from(metersCandidate: Int): Either[OutOfBoundsError, Meters] = metersCandidate match
       case meters if meters < 0 => Left(OutOfBoundsError(s"Invalid meters value $meters"))
       case meters               => Right(meters)
 
+    /**
+     * Method that builds a Meters from an int at compile time. This method is used when working with fixed values (''magic numbers'') in the code.
+     * The compiler will warn us if the value is invalid at compile time.
+     *
+     * @param meters
+     *   the given Int.
+     * @return
+     *   A valid Meters or a compiler error.
+     * @see
+     *   More info at [[https://docs.scala-lang.org/scala3/reference/metaprogramming/inline.html]]
+     */
     inline def apply(meters: Int): Meters =
       requireConst(meters)
       inline if meters < 0
